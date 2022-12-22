3756f016213b4cac2a356def5c4dca278a25e4d3
Resla2 <116928691+Resla2@users.noreply.github.com>
2022-12-22 02:00:37 -0500

    Looping Lab

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function writeCards(names, event) {
-    let thankYouCards = []
-    for (let i = 0; i < names.length; i++) {
-      thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
-    }
-    return thankYouCards
+  let thanksCards = []
+  for (let i = 0; i < names.length; i++) {
+    thanksCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
-  
-  function countDown(Number) {
-    while (Number > 0) {
-      console.log(Number);
-      Number -= 1;
-    }
+  return thanksCards
+}
+
+function countDown(Number) {
+  while (Number > 0) {
     console.log(Number);
+    Number -= 1;
   }
+  console.log(Number);
+}
